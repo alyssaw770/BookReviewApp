ba1000b60da0eddcaf97c892b7d979f3711a7848
alyssaw770 <alyssaw770@email.com>
2024-10-20 14:31:49 -0500

    inital project

---
+++ api/Models/Books.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+    }
+}
